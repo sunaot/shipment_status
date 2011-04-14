d620a1afc0a1bff4f88c1c439dc0149ac1de2d14
Sunao Tanabe <sunao.tanabe@gmail.com>
2011-04-14 20:00:58 +0900

    codes loops for APIs limits.

--- lib/yamato_delivery_status.rb
+++ lib/yamato_delivery_status.rb
@@ -12,30 +12,53 @@ class Hash
   alias to_qs to_query_string
 end
 
-# thanks to Ryan Heath
-#  - http://rpheath.com/posts/341-ruby-inject-with-index
-module Enumerable
-  def inject_with_index(injected)
-    each_with_index{ |obj, index| injected = yield(injected, obj, index) }
-    injected
+module ShippingCarrier
+  class ShipmentStatusAPI
+    def ask
+      raise('too many bill numbers') unless bill_numbers.size <= 10
+      parameterize = proc {|numbers| numbers.each.with_index.inject({}) {|r, (n, i)| r["number#{'%02d' % (i+1)}".to_sym] = n; r} }
+      Net::HTTP.version_1_2
+      Net::HTTP.start('toi.kuronekoyamato.co.jp', 80) do |http|
+        param = {number00: 2, number01: '',
+                              number02: '', 
+                              number03: '', 
+                              number04: '', 
+                              number05: '', 
+                              number07: '', 
+                              number08: '', 
+                              number09: '', 
+                              number10: '' 
+                }.update(parameterize.call(bill_numbers))
+        response = http.post('/cgi-bin/tneko', param.to_qs)
+        response.body.force_encoding('Shift_JIS').encode('UTF-8')
+      end
+      Hash[*(Nokogiri::HTML(ask_delivery_status.call(bill_numbers)).search("//td[@class='denpyo' or @class='ct']").map {|n| n.content.strip})]
+    end
   end
-end
+  class ShipmentStatus
+    class Unreachable < Exception; end
+    def search(codes)
+      raise Unreachable if codes.include? :timeout
+      each_bulk(codes).map do |bulk_codes|
+        @status.ask bulk_codes
+      end.reduce(:merge)
+    end
+  
+    def each_bulk(codes)
+      parts = (codes.size / 10) + 1
+      codes.group_by.with_index {|n, i| i % parts }
+    end
+  
+    def initialize(status)
+      @status = status || ShipmentStatusAPI.new
+    end
+  end
 
-class YamatoDeliveryStatus
-  class Unreachable < Exception; end
-  def search(codes)
-    raise Unreachable if codes.include? :timeout
-    return @status[codes.shift] if @status
-  end
-
-  def initialize(status)
-    @status = status if status
-  end
 end
 
 ask_delivery_status = lambda do |bill_numbers|
   raise('too many bill numbers') unless bill_numbers.size <= 10
-  parameterize = proc {|numbers| numbers.inject_with_index({}) {|r, n, i| r["number#{'%02d' % (i+1)}".to_sym] = n; r} }
+  parameterize = proc {|numbers| numbers.each.with_index.inject({}) {|r, (n, i)| r["number#{'%02d' % (i+1)}".to_sym] = n; r} }
   Net::HTTP.version_1_2
   Net::HTTP.start('toi.kuronekoyamato.co.jp', 80) {|http|
     param = {number00: 2, number01: '',
--- spec/yamato_delivery_status_spec.rb
+++ spec/yamato_delivery_status_spec.rb
@@ -1,14 +1,15 @@
 # coding: utf-8
 require 'yamato_delivery_status'
-describe YamatoDeliveryStatus, '問い合わせ番号ごとに現在の配送状況を返すこと' do 
+include ShippingCarrier
+describe ShipmentStatus, '問い合わせ番号ごとに現在の配送状況を返すこと' do 
   before do
-    status_database = {a: {a: 'OK'}, b: {b: 'NOT FOUND'} }
-    @status = YamatoDeliveryStatus.new(status_database)
+    status = double('status')
+    status.stub(:ask) { {a: 'OK', b: 'NOT FOUND'} }
+    @status = ShipmentStatus.new(status)
   end
-  it { @status.search([:a]).should == {a: 'OK'} }
-  it { @status.search([:b]).should == {b: 'NOT FOUND'} }
+  it { @status.search([:a, :b]).should == {a: 'OK', b: 'NOT FOUND'} }
   context '配送状況を問い合わせできないとき' do 
-    it { proc { @status.search([:timeout]) }.should raise_error(YamatoDeliveryStatus::Unreachable) }
+    it { proc { @status.search([:timeout]) }.should raise_error(ShipmentStatus::Unreachable) }
   end
 end
 
